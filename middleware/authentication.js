dcdc3df517604aee7ec2f9a1860272f133d45e54
dhrubobarman <dhrubobarman847@gmail.com>
2023-02-15 20:19:27 +0530

    first Commit

---
+++ middleware/authentication.js
@@ -0,0 +1,34 @@
+const CustomError = require("../errors");
+const { isTokenValid } = require("../utils");
+
+const authenticateUser = async (req, res, next) => {
+  const token = req.signedCookies.token;
+  if (!token) {
+    throw new CustomError.UnauthenticatedError("Authenticatoin Invalid");
+  }
+
+  try {
+    const { name, userId, role } = isTokenValid({ token });
+    req.user = { name, userId, role };
+    next();
+  } catch (error) {
+    console.log(error);
+    throw new CustomError.UnauthenticatedError("Authenticatoin Invalid");
+  }
+};
+
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError(
+        "Unauthorized to access this route"
+      );
+    }
+    next();
+  };
+};
+
+module.exports = {
+  authenticateUser,
+  authorizePermissions,
+};
---
+++ models/Product.js
@@ -0,0 +1,91 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide product name"],
+      maxLength: [150, "Product name cannot be more than 150 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please provide product price"],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide product description"],
+      maxLength: [
+        1000,
+        "Product description cannot be more than 1000 characters",
+      ],
+    },
+    image: {
+      type: String,
+      default: "/uploads/example.jepg",
+    },
+    category: {
+      type: String,
+      required: [true, "Please provide product category"],
+      enum: ["office", "kitchen", "bedroom"],
+    },
+    company: {
+      type: String,
+      required: [true, "Please provide product company"],
+      enum: {
+        values: ["ikea", "liddy", "marcos"],
+        message: "{VALUE} is not supported",
+      },
+    },
+    colors: {
+      type: [String],
+      default: ["#222"],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timeStamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+ProductSchema.virtual("reviews", {
+  ref: "Review",
+  localField: "_id",
+  foreignField: "product",
+  justOne: false,
+});
+
+ProductSchema.pre("remove", async function (next) {
+  await this.model("Review").deleteMany({ product: this._id });
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
